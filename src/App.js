f4533837178371c11489f3b06df134b03129f808
5sarad <saradpoudel05@gmail.com>
2024-03-17 15:31:12 +1100

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -14,14 +14,24 @@ function GithubUser({name, location, avatar}) {
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
   useEffect(()=>{
+    setLoading(true);
     fetch(
       `https://api.github.com/users/5sarad`
     )
     .then((response) => response.json())
-    .then(setData);
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
   }, []);
-  if(data) 
+  
+  if(loading) return <h1>Loading.......</h1>
+  if(error)
+    return <pre>{JSON.stringify(error)}</pre>
+  if(!data) return null;
+
   return (
     <GithubUser 
       name={data.name}
@@ -29,7 +39,7 @@ function App() {
       avatar={data.avatar_url}
     />
   );
-  return <h1>Data</h1>
+  
     
 }
 
